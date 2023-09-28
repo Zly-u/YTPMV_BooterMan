966d527a9bf1a814794ed892c928eb74b82ac419
Zly <zlyudchik@gmail.com>
2023-09-28 17:20:21 +0300

    - Shader changes for scrolling with mirroring - BG in menu scrolls

--- scenes/MENU.gd
+++ scenes/MENU.gd
@@ -3,7 +3,9 @@ extends Control
 @export var game_scene: PackedScene = null
 @export var credits_scene: PackedScene = null
 @export var controls_scene: PackedScene = null
-@export var field_offset_speed = Vector2(0, 0)
+
+@export var angle_dir: float = 0
+@export var field_offset_speed: float = 0
 
 func _ready() -> void:
 	%AnimatedSprite2D2.play("default")
@@ -12,7 +14,12 @@ func _ready() -> void:
 		g_MenuAmbient.play()
 
 func _process(delta: float) -> void:
-	%BgField.region_rect.position += field_offset_speed * delta
+	var dir: Vector2 = Vector2(1, 0).rotated(deg_to_rad(angle_dir))
+#	%BgField.position.x = fmod(%BgField.position.x + dir.x * delta, 1.0)
+#	%BgField.position.y = fmod(%BgField.position.y + dir.y * delta, 1.0)
+	%BgField.region_rect.position -= dir * field_offset_speed * delta
+#
+#	print(%BgField.position)
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_packed(game_scene)
